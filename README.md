a7009a994a35b98d2a877b2fef6c1e19f7f91bcd
dominic monaco <dominicmonaco@MacBook-Pro.localdomain>
2024-01-18 13:32:30 -0500

    add initial scaffolding of application

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ls
 Python flask app that functions like the ls cmd
+
+## Parameters of api
+GET / -> list contents of root directory
+GET /bar -> list contents of /bar/ directory
+GET /foo1.txt -> contents of file foo1.txt
+GET /bar/bar1 -> contents of file bar/bar1
+POST /new_file.txt -> create a new file /new_file.txt
+
+Files in directories will include name, type, size, and owner information.
+Assumes all file content will fit in a JSON blob and still be readable.
---
+++ webserver.py
@@ -0,0 +1,61 @@
+import os
+import json
+import humanize
+from flask import Flask, request, jsonify
+from pwd import getpwuid
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_root():
+    return jsonify(loop_dir('/'))
+
+@app.route('/<path:path>', methods=['GET'])
+def get_path(path):
+    full_path = os.path.join('/', path)
+    if os.path.isdir(full_path):
+        return jsonify(loop_dir(full_path))
+    elif os.path.isfile(full_path):
+        with open(full_path, 'r') as f:
+            return f.read()
+    else:
+        return 'Not found', 404
+
+@app.route('/<path:path>', methods=['POST'])
+def post_new_file(): 
+    full_path = os.path.join('/', path)
+    if os.path.exists(full_path):
+        return 'Conflict: file already exists', 409
+    else:
+        with open(full_path, 'w') as f:
+            f.write(request.data.decode())
+        return 'Created', 201, {'Location': full_path}
+
+def loop_dir(full_path):
+    output = []
+    for x in os.listdir(full_path):
+        x_path = os.path.join(full_path, x)
+        if os.path.isfile(x_path):
+            output.append({x: get_file_details(x_path)})
+        else:
+            output.append(x)
+    return output
+
+def get_file_details(filename):
+    return {
+        "Filetype": os.path.splitext(filename)[1],
+        "Size": humanize.naturalsize(os.path.getsize(filename)),
+        "Owner": getpwuid(os.stat(filename).st_uid).pw_name
+    }
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print('Usage: python webserver.py /path/to/serve/from [port]')
+        sys.exit(1)
+    path = sys.argv[1]
+    port = int(sys.argv[2]) if len(sys.argv) > 2 else 8000
+    os.chdir(path)
+    app.run(port=port)
